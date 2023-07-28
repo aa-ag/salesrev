3ffe210712606dec6166fe1e14879bf6418fbd38
aa-ag <aaron.aguerrevere@gmail.com>
2023-07-28 14:58:24 -0500

    Check imported pacakges logic

--- code/rev.py
+++ code/rev.py
@@ -1,4 +1,5 @@
 ############ ------------ IMPORTS ------------##################################
+import importlib.util as u
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
@@ -12,4 +13,11 @@ from matplotlib import pyplot as plt
 
 # ------------ DRIVER CODE ------------##############################ß
 if __name__ == "__main__":
-    pass
+    pckgs = ["pandas", "numpy", "matplotlib"]
+    print("\n")
+    for pckg in pckgs:
+        spec = u.find_spec(pckg)
+        if spec is None:
+            print(f'"{pckg}" is not installed')
+        else:
+            print(f'"{pckg}" is installed')
