cddde122b941bef3b94622cb7c4e56708e2ecf98
aa-ag <aaron.aguerrevere@gmail.com>
2023-07-28 22:20:13 -0500

    Add plotting relationsip between x vars and y vars

--- code/rev.py
+++ code/rev.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 
 ############ ------------ FUNCTION(S) ------------##############################
 def test_env():
+    # TODO: this will be moved to "tests" eventually
     pckgs = ["pandas", "numpy", "matplotlib", "seaborn"]
     print("\n")
     for pckg in pckgs:
@@ -30,15 +31,27 @@ def check_inputs():
 
 def clean_inputs():
     df = pd.read_csv("files/advertising.csv")
-    df.drop(["Unnamed: 0"], axis=1, inplace=True)
+    df.drop(["N"], axis=1, inplace=True)
     return df.head()
 
 
-def analyse(data):
+def initial_analysis(data):
     dataframe_columns = data.columns.to_list()
     for each_column in dataframe_columns:
         fig = sns.displot(data[each_column], kind="kde")
         fig.savefig(f"./files/{each_column}_plot.png")
+
+
+def explore_predictor_response_relationship(data):
+    sns.pairplot(
+        data,
+        x_vars=["TV", "radio", "newspaper"],
+        y_vars="sales",
+        height=7,
+        aspect=1,
+        kind="reg"
+    )
+    plt.show()
 
 
 # ------------ DRIVER CODE ------------##############################ß
@@ -46,4 +59,5 @@ if __name__ == "__main__":
     if test_env():
         # check_inputs()
         data = clean_inputs()
-        analyse(data)
+        # initial_analysis(data)
+        explore_predictor_response_relationship(data)
