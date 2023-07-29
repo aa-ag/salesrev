ddbc0c20f80b209234ba3e34908f7d2d4d636109
aa-ag <aaron.aguerrevere@gmail.com>
2023-07-28 21:47:13 -0500

    Update analyse to store plot as image, uchange kind within analyse & inputs folder name

--- code/rev.py
+++ code/rev.py
@@ -1,4 +1,5 @@
 ############ ------------ IMPORTS ------------##################################
+import importlib.util as u
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -7,7 +8,7 @@ from matplotlib import pyplot as plt
 
 ############ ------------ FUNCTION(S) ------------##############################
 def test_env():
-    pckgs = ["pandas", "numpy", "matplotlib", "sns"]
+    pckgs = ["pandas", "numpy", "matplotlib", "seaborn"]
     print("\n")
     for pckg in pckgs:
         spec = u.find_spec(pckg)
@@ -21,20 +22,27 @@ def test_env():
 
 
 def check_inputs():
-    df = pd.read_csv("inputs/advertising.csv")
+    df = pd.read_csv("files/advertising.csv")
     print(df.info(verbose=True))
     print(df.describe())
     print(df.head(5))
 
 
 def clean_inputs():
-    df = pd.read_csv("inputs/advertising.csv")
+    df = pd.read_csv("files/advertising.csv")
     df.drop(["Unnamed: 0"], axis=1, inplace=True)
-    print(df.head())
+    return df.head()
+
+
+def analyse(data):
+    # plt.show()
+    fig = sns.displot(data.Sales, kind="kde")
+    fig.savefig("./files/sales_plot.png")
 
 
 # ------------ DRIVER CODE ------------##############################ß
 if __name__ == "__main__":
     if test_env():
         # check_inputs()
-        clean_inputs()
+        data = clean_inputs()
+        analyse(data)
